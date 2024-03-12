d7d1d1652282c5f8db7fa28c4462652cd8b34889
jodosjodos <jeandedieu2030@gmail.com>
2024-03-12 10:02:41 +0200

    [migrate] move to prisma

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,50 +1,40 @@
-import { sql } from '@vercel/postgres';
-import {
-  CustomerField,
-  CustomersTableType,
-  InvoiceForm,
-  InvoicesTable,
-  LatestInvoiceRaw,
-  User,
-  Revenue,
-} from './definitions';
-import { formatCurrency } from './utils';
+// Import PrismaClient
+import { PrismaClient } from '@prisma/client';
 
+// Initialize PrismaClient
+const prisma = new PrismaClient();
+
+// Update fetchRevenue function
 export async function fetchRevenue() {
-  // Add noStore() here to prevent the response from being cached.
-  // This is equivalent to in fetch(..., {cache: 'no-store'}).
-
   try {
-    // Artificially delay a response for demo purposes.
-    // Don't do this in production :)
-
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
-
-    const data = await sql<Revenue>`SELECT * FROM revenue`;
-
-    // console.log('Data fetch completed after 3 seconds.');
-
-    return data.rows;
+    const revenueData = await prisma.revenue.findMany();
+    return revenueData;
   } catch (error) {
     console.error('Database Error:', error);
     throw new Error('Failed to fetch revenue data.');
   }
 }
 
+// Update fetchLatestInvoices function
 export async function fetchLatestInvoices() {
   try {
-    const data = await sql<LatestInvoiceRaw>`
-      SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
-      FROM invoices
-      JOIN customers ON invoices.customer_id = customers.id
-      ORDER BY invoices.date DESC
-      LIMIT 5`;
-
-    const latestInvoices = data.rows.map((invoice) => ({
-      ...invoice,
-      amount: formatCurrency(invoice.amount),
-    }));
+    const latestInvoices = await prisma.invoice.findMany({
+      select: {
+        id: true,
+        amount: true,
+        date: true,
+        status: true,
+        customer: {
+          select: {
+            name: true,
+            email: true,
+            imageUrl: true,
+          },
+        },
+      },
+      orderBy: { date: 'desc' },
+      take: 5,
+    });
     return latestInvoices;
   } catch (error) {
     console.error('Database Error:', error);
@@ -52,91 +42,95 @@ export async function fetchLatestInvoices() {
   }
 }
 
+// Update fetchCardData function
 export async function fetchCardData() {
   try {
-    // You can probably combine these into a single SQL query
-    // However, we are intentionally splitting them to demonstrate
-    // how to initialize multiple queries in parallel with JS.
-    const invoiceCountPromise = sql`SELECT COUNT(*) FROM invoices`;
-    const customerCountPromise = sql`SELECT COUNT(*) FROM customers`;
-    const invoiceStatusPromise = sql`SELECT
-         SUM(CASE WHEN status = 'paid' THEN amount ELSE 0 END) AS "paid",
-         SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
-         FROM invoices`;
-
-    const data = await Promise.all([
-      invoiceCountPromise,
-      customerCountPromise,
-      invoiceStatusPromise,
-    ]);
-
-    const numberOfInvoices = Number(data[0].rows[0].count ?? '0');
-    const numberOfCustomers = Number(data[1].rows[0].count ?? '0');
-    const totalPaidInvoices = formatCurrency(data[2].rows[0].paid ?? '0');
-    const totalPendingInvoices = formatCurrency(data[2].rows[0].pending ?? '0');
+    const numberOfInvoices = await prisma.invoice.count();
+    const numberOfCustomers = await prisma.customer.count();
+    const totalPaidInvoices = await prisma.invoice.aggregate({
+      _sum: {
+        amount: true,
+      },
+      where: {
+        status: 'pending',
+      },
+    });
+    const totalPendingInvoices = await prisma.invoice.aggregate({
+      _sum: {
+        amount: true,
+      },
+      where: {
+        status: 'pending',
+      },
+    });
 
     return {
       numberOfCustomers,
       numberOfInvoices,
-      totalPaidInvoices,
-      totalPendingInvoices,
+      totalPaidInvoices: totalPaidInvoices._sum.amount,
+      totalPendingInvoices: totalPendingInvoices._sum.amount,
     };
   } catch (error) {
     console.error('Database Error:', error);
     throw new Error('Failed to fetch card data.');
   }
 }
-
 const ITEMS_PER_PAGE = 6;
+
+// Update fetchFilteredInvoices function
 export async function fetchFilteredInvoices(
   query: string,
   currentPage: number,
 ) {
+  const ITEMS_PER_PAGE = 6;
   const offset = (currentPage - 1) * ITEMS_PER_PAGE;
 
   try {
-    const invoices = await sql<InvoicesTable>`
-      SELECT
-        invoices.id,
-        invoices.amount,
-        invoices.date,
-        invoices.status,
-        customers.name,
-        customers.email,
-        customers.image_url
-      FROM invoices
-      JOIN customers ON invoices.customer_id = customers.id
-      WHERE
-        customers.name ILIKE ${`%${query}%`} OR
-        customers.email ILIKE ${`%${query}%`} OR
-        invoices.amount::text ILIKE ${`%${query}%`} OR
-        invoices.date::text ILIKE ${`%${query}%`} OR
-        invoices.status ILIKE ${`%${query}%`}
-      ORDER BY invoices.date DESC
-      LIMIT ${ITEMS_PER_PAGE} OFFSET ${offset}
-    `;
+    const filteredInvoices = await prisma.invoice.findMany({
+      where: {
+        OR: [
+          { amount: parseInt(query) || undefined }, // Parse query to number for amount
+          { status: { contains: query } },
+          { customer: { name: { contains: query } } },
+          { customer: { email: { contains: query } } },
+        ],
+      },
+      include: {
+        customer: {
+          select: {
+            name: true,
+            email: true,
+            imageUrl: true,
+          },
+        },
+      },
+      orderBy: { date: 'desc' },
+      take: ITEMS_PER_PAGE,
+      skip: offset,
+    });
 
-    return invoices.rows;
+    return filteredInvoices;
   } catch (error) {
     console.error('Database Error:', error);
     throw new Error('Failed to fetch invoices.');
   }
 }
-
+// Update fetchInvoicesPages function
 export async function fetchInvoicesPages(query: string) {
   try {
-    const count = await sql`SELECT COUNT(*)
-    FROM invoices
-    JOIN customers ON invoices.customer_id = customers.id
-    WHERE
-      customers.name ILIKE ${`%${query}%`} OR
-      customers.email ILIKE ${`%${query}%`} OR
-      invoices.amount::text ILIKE ${`%${query}%`} OR
-      invoices.date::text ILIKE ${`%${query}%`} OR
-      invoices.status ILIKE ${`%${query}%`}
-  `;
+    const count = await prisma.invoice.count({
+      where: {
+        OR: [
+          { amount:  parseInt( query) || undefined },
+          { status: { contains: query } },
+          { customer: { name: { contains: query } } },
+          { customer: { email: { contains: query } } },
+        ],
+      },
+    });
+    const ITEMS_PER_PAGE = 6;
 
-    const totalPages = Math.ceil(Number(count.rows[0].count) / ITEMS_PER_PAGE);
+    const totalPages = Math.ceil(count / ITEMS_PER_PAGE);
     return totalPages;
   } catch (error) {
     console.error('Database Error:', error);
@@ -144,88 +138,21 @@ export async function fetchInvoicesPages(query: string) {
   }
 }
 
+// Update fetchInvoiceById function
 export async function fetchInvoiceById(id: string) {
   try {
-    const data = await sql<InvoiceForm>`
-      SELECT
-        invoices.id,
-        invoices.customer_id,
-        invoices.amount,
-        invoices.status
-      FROM invoices
-      WHERE invoices.id = ${id};
-    `;
+    const invoice = await prisma.invoice.findUnique({
+      where: {
+        id: id,
+      },
+    });
 
-    const invoice = data.rows.map((invoice) => ({
-      ...invoice,
-      // Convert amount from cents to dollars
-      amount: invoice.amount / 100,
-    }));
+    if (!invoice) {
+      throw new Error('Invoice not found.');
+    }
 
-    return invoice[0];
+    return invoice;
   } catch (error) {
     console.error('Database Error:', error);
-    throw new Error('Failed to fetch invoice.');
-  }
-}
-
-export async function fetchCustomers() {
-  try {
-    const data = await sql<CustomerField>`
-      SELECT
-        id,
-        name
-      FROM customers
-      ORDER BY name ASC
-    `;
-
-    const customers = data.rows;
-    return customers;
-  } catch (err) {
-    console.error('Database Error:', err);
-    throw new Error('Failed to fetch all customers.');
-  }
-}
-
-export async function fetchFilteredCustomers(query: string) {
-  try {
-    const data = await sql<CustomersTableType>`
-		SELECT
-		  customers.id,
-		  customers.name,
-		  customers.email,
-		  customers.image_url,
-		  COUNT(invoices.id) AS total_invoices,
-		  SUM(CASE WHEN invoices.status = 'pending' THEN invoices.amount ELSE 0 END) AS total_pending,
-		  SUM(CASE WHEN invoices.status = 'paid' THEN invoices.amount ELSE 0 END) AS total_paid
-		FROM customers
-		LEFT JOIN invoices ON customers.id = invoices.customer_id
-		WHERE
-		  customers.name ILIKE ${`%${query}%`} OR
-        customers.email ILIKE ${`%${query}%`}
-		GROUP BY customers.id, customers.name, customers.email, customers.image_url
-		ORDER BY customers.name ASC
-	  `;
-
-    const customers = data.rows.map((customer) => ({
-      ...customer,
-      total_pending: formatCurrency(customer.total_pending),
-      total_paid: formatCurrency(customer.total_paid),
-    }));
-
-    return customers;
-  } catch (err) {
-    console.error('Database Error:', err);
-    throw new Error('Failed to fetch customer table.');
-  }
-}
-
-export async function getUser(email: string) {
-  try {
-    const user = await sql`SELECT * FROM users WHERE email=${email}`;
-    return user.rows[0] as User;
-  } catch (error) {
-    console.error('Failed to fetch user:', error);
-    throw new Error('Failed to fetch user.');
   }
 }
--- scripts/seed.js
+++ scripts/seed.js
@@ -1,177 +1,305 @@
-const { db } = require('@vercel/postgres');
+// const { db } = require('@vercel/postgres');
+// const {
+//   invoices,
+//   customers,
+//   revenue,
+//   users,
+// } = require('../app/lib/placeholder-data.js');
+// const bcrypt = require('bcrypt');
+// async function seedUsers(client) {
+//   try {
+//     await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+//     // Create the "users" table if it doesn't exist
+//     const createTable = await client.sql`
+//       CREATE TABLE IF NOT EXISTS users (
+//         id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+//         name VARCHAR(255) NOT NULL,
+//         email TEXT NOT NULL UNIQUE,
+//         password TEXT NOT NULL
+//       );
+//     `;
+//     console.log(`Created "users" table`);
+
+//     // Insert data into the "users" table
+//     const insertedUsers = await Promise.all(
+//       users.map(async (user) => {
+//         const hashedPassword = await bcrypt.hash(user.password, 10);
+//         return client.sql`
+//         INSERT INTO users (id, name, email, password)
+//         VALUES (${user.id}, ${user.name}, ${user.email}, ${hashedPassword})
+//         ON CONFLICT (id) DO NOTHING;
+//       `;
+//       }),
+//     );
+
+//     console.log(`Seeded ${insertedUsers.length} users`);
+
+//     return {
+//       createTable,
+//       users: insertedUsers,
+//     };
+//   } catch (error) {
+//     console.error('Error seeding users:', error);
+//     throw error;
+//   }
+// }
+
+// async function seedInvoices(client) {
+//   try {
+//     await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+
+//     // Create the "invoices" table if it doesn't exist
+//     const createTable = await client.sql`
+//     CREATE TABLE IF NOT EXISTS invoices (
+//     id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+//     customer_id UUID NOT NULL,
+//     amount INT NOT NULL,
+//     status VARCHAR(255) NOT NULL,
+//     date DATE NOT NULL
+//   );
+// `;
+
+//     console.log(`Created "invoices" table`);
+
+//     // Insert data into the "invoices" table
+//     const insertedInvoices = await Promise.all(
+//       invoices.map(
+//         (invoice) => client.sql`
+//         INSERT INTO invoices (customer_id, amount, status, date)
+//         VALUES (${invoice.customer_id}, ${invoice.amount}, ${invoice.status}, ${invoice.date})
+//         ON CONFLICT (id) DO NOTHING;
+//       `,
+//       ),
+//     );
+
+//     console.log(`Seeded ${insertedInvoices.length} invoices`);
+
+//     return {
+//       createTable,
+//       invoices: insertedInvoices,
+//     };
+//   } catch (error) {
+//     console.error('Error seeding invoices:', error);
+//     throw error;
+//   }
+// }
+
+// async function seedCustomers(client) {
+//   try {
+//     await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+
+//     // Create the "customers" table if it doesn't exist
+//     const createTable = await client.sql`
+//       CREATE TABLE IF NOT EXISTS customers (
+//         id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+//         name VARCHAR(255) NOT NULL,
+//         email VARCHAR(255) NOT NULL,
+//         image_url VARCHAR(255) NOT NULL
+//       );
+//     `;
+
+//     console.log(`Created "customers" table`);
+
+//     // Insert data into the "customers" table
+//     const insertedCustomers = await Promise.all(
+//       customers.map(
+//         (customer) => client.sql`
+//         INSERT INTO customers (id, name, email, image_url)
+//         VALUES (${customer.id}, ${customer.name}, ${customer.email}, ${customer.image_url})
+//         ON CONFLICT (id) DO NOTHING;
+//       `,
+//       ),
+//     );
+
+//     console.log(`Seeded ${insertedCustomers.length} customers`);
+
+//     return {
+//       createTable,
+//       customers: insertedCustomers,
+//     };
+//   } catch (error) {
+//     console.error('Error seeding customers:', error);
+//     throw error;
+//   }
+// }
+
+// async function seedRevenue(client) {
+//   try {
+//     // Create the "revenue" table if it doesn't exist
+//     const createTable = await client.sql`
+//       CREATE TABLE IF NOT EXISTS revenue (
+//         month VARCHAR(4) NOT NULL UNIQUE,
+//         revenue INT NOT NULL
+//       );
+//     `;
+
+//     console.log(`Created "revenue" table`);
+
+//     // Insert data into the "revenue" table
+//     const insertedRevenue = await Promise.all(
+//       revenue.map(
+//         (rev) => client.sql`
+//         INSERT INTO revenue (month, revenue)
+//         VALUES (${rev.month}, ${rev.revenue})
+//         ON CONFLICT (month) DO NOTHING;
+//       `,
+//       ),
+//     );
+
+//     console.log(`Seeded ${insertedRevenue.length} revenue`);
+
+//     return {
+//       createTable,
+//       revenue: insertedRevenue,
+//     };
+//   } catch (error) {
+//     console.error('Error seeding revenue:', error);
+//     throw error;
+//   }
+// }
+
+// async function main() {
+//   const client = await db.connect();
+
+//   await seedUsers(client);
+//   await seedCustomers(client);
+//   await seedInvoices(client);
+//   await seedRevenue(client);
+
+//   await client.end();
+// }
+
+// main().catch((err) => {
+//   console.error(
+//     'An error occurred while attempting to seed the database:',
+//     err,
+//   );
+// });
+
+const { PrismaClient } = require('@prisma/client');
 const {
   invoices,
   customers,
   revenue,
   users,
 } = require('../app/lib/placeholder-data.js');
-const bcrypt = require('bcrypt');
-async function seedUsers(client) {
-  try {
-    await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
-    // Create the "users" table if it doesn't exist
-    const createTable = await client.sql`
-      CREATE TABLE IF NOT EXISTS users (
-        id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-        name VARCHAR(255) NOT NULL,
-        email TEXT NOT NULL UNIQUE,
-        password TEXT NOT NULL
-      );
-    `;
-    console.log(`Created "users" table`);
+const { User } = require('@prisma/client');
+const prisma = new PrismaClient();
 
-    // Insert data into the "users" table
+const bcrypt = require('bcrypt');
+async function seedUsers() {
+  try {
     const insertedUsers = await Promise.all(
       users.map(async (user) => {
         const hashedPassword = await bcrypt.hash(user.password, 10);
-        return client.sql`
-        INSERT INTO users (id, name, email, password)
-        VALUES (${user.id}, ${user.name}, ${user.email}, ${hashedPassword})
-        ON CONFLICT (id) DO NOTHING;
-      `;
+        return prisma.user.create({
+          data: {
+            password: hashedPassword,
+            name: user.name,
+            email: user.email,
+          },
+        });
       }),
     );
-
     console.log(`Seeded ${insertedUsers.length} users`);
-
-    return {
-      createTable,
-      users: insertedUsers,
-    };
+    return insertedUsers;
   } catch (error) {
     console.error('Error seeding users:', error);
     throw error;
   }
 }
 
-async function seedInvoices(client) {
+// Function to seed customers
+async function seedCustomers() {
   try {
-    await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
-
-    // Create the "invoices" table if it doesn't exist
-    const createTable = await client.sql`
-    CREATE TABLE IF NOT EXISTS invoices (
-    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-    customer_id UUID NOT NULL,
-    amount INT NOT NULL,
-    status VARCHAR(255) NOT NULL,
-    date DATE NOT NULL
-  );
-`;
-
-    console.log(`Created "invoices" table`);
-
-    // Insert data into the "invoices" table
-    const insertedInvoices = await Promise.all(
-      invoices.map(
-        (invoice) => client.sql`
-        INSERT INTO invoices (customer_id, amount, status, date)
-        VALUES (${invoice.customer_id}, ${invoice.amount}, ${invoice.status}, ${invoice.date})
-        ON CONFLICT (id) DO NOTHING;
-      `,
-      ),
-    );
-
-    console.log(`Seeded ${insertedInvoices.length} invoices`);
-
-    return {
-      createTable,
-      invoices: insertedInvoices,
-    };
-  } catch (error) {
-    console.error('Error seeding invoices:', error);
-    throw error;
-  }
-}
-
-async function seedCustomers(client) {
-  try {
-    await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
-
-    // Create the "customers" table if it doesn't exist
-    const createTable = await client.sql`
-      CREATE TABLE IF NOT EXISTS customers (
-        id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-        name VARCHAR(255) NOT NULL,
-        email VARCHAR(255) NOT NULL,
-        image_url VARCHAR(255) NOT NULL
-      );
-    `;
-
-    console.log(`Created "customers" table`);
-
-    // Insert data into the "customers" table
     const insertedCustomers = await Promise.all(
-      customers.map(
-        (customer) => client.sql`
-        INSERT INTO customers (id, name, email, image_url)
-        VALUES (${customer.id}, ${customer.name}, ${customer.email}, ${customer.image_url})
-        ON CONFLICT (id) DO NOTHING;
-      `,
-      ),
+      customers.map(async (customer) => {
+        return prisma.customer.create({
+          data: {
+            name: customer.name,
+            email: customer.email,
+            imageUrl: customer.image_url,
+          },
+        });
+      })
     );
-
     console.log(`Seeded ${insertedCustomers.length} customers`);
-
-    return {
-      createTable,
-      customers: insertedCustomers,
-    };
+    return insertedCustomers;
   } catch (error) {
     console.error('Error seeding customers:', error);
     throw error;
   }
 }
 
-async function seedRevenue(client) {
+// Function to seed invoices
+async function seedInvoices() {
   try {
-    // Create the "revenue" table if it doesn't exist
-    const createTable = await client.sql`
-      CREATE TABLE IF NOT EXISTS revenue (
-        month VARCHAR(4) NOT NULL UNIQUE,
-        revenue INT NOT NULL
-      );
-    `;
+    // Fetch all customers
+    const allCustomers = await prisma.customer.findMany();
 
-    console.log(`Created "revenue" table`);
+    // Extract customer IDs
+    const customerIds = allCustomers.map((customer) => customer.id);
 
-    // Insert data into the "revenue" table
-    const insertedRevenue = await Promise.all(
-      revenue.map(
-        (rev) => client.sql`
-        INSERT INTO revenue (month, revenue)
-        VALUES (${rev.month}, ${rev.revenue})
-        ON CONFLICT (month) DO NOTHING;
-      `,
-      ),
+    const insertedInvoices = await Promise.all(
+      invoices.map(async (invoice, index) => {
+        // Use the customer IDs obtained from the database
+        const customerId = customerIds[index % customerIds.length];
+
+        return prisma.invoice.create({
+          data: {
+            customerId: customerId,
+            amount: invoice.amount,
+            status: invoice.status,
+            date: new Date(invoice.date),
+          },
+        });
+      })
     );
 
-    console.log(`Seeded ${insertedRevenue.length} revenue`);
+    console.log(`Seeded ${insertedInvoices.length} invoices`);
+    return insertedInvoices;
+  } catch (error) {
+    console.error('Error seeding invoices:', error);
+    throw error;
+  }
+}
 
-    return {
-      createTable,
-      revenue: insertedRevenue,
-    };
+
+
+// Function to seed revenue
+async function seedRevenue() {
+  try {
+    const insertedRevenue = await Promise.all(
+      revenue.map(async (rev) => {
+        return prisma.revenue.create({
+          data: {
+            month: rev.month,
+            revenue: rev.revenue,
+          }
+        });
+      })
+    );
+    console.log(`Seeded ${insertedRevenue.length} revenue records`);
+    return insertedRevenue;
   } catch (error) {
     console.error('Error seeding revenue:', error);
     throw error;
   }
 }
 
-async function main() {
-  const client = await db.connect();
 
-  await seedUsers(client);
-  await seedCustomers(client);
-  await seedInvoices(client);
-  await seedRevenue(client);
-
-  await client.end();
+async function main(){
+  await seedUsers();
+  await seedCustomers();
+  await seedInvoices();
+  await seedRevenue();
 }
 
-main().catch((err) => {
+main().then(()=>{
+  console.log("data have been inserted successfully");
+}).catch(err=>{
   console.error(
-    'An error occurred while attempting to seed the database:',
-    err,
-  );
-});
+         'An error occurred while attempting to seed the database:',
+        err,
+      );
+})
