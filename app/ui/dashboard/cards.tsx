dc2e6c8cf312865123facda41f36feffee891498
jodosjodos <jeandedieu2030@gmail.com>
2024-03-16 18:52:57 +0200

    [fix] style files3

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -26,6 +26,7 @@ export default async function CardWrapper() {
 
 {/* card */}
       <Card title="Collected" value={totalPaidInvoices} type="collected" />
+      {/* card 2 */}
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
       <Card
